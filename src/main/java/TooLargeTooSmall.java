0222ef5c440d8e41c207baebba1b2db3968f4dea
igordon215 <igordon215@gmail.com>
2024-07-07 19:02:15 -0400

    lab done

---
+++ src/main/java/TooLargeTooSmall.java
@@ -0,0 +1,30 @@
+/**
+ * Created by iyasuwatts on 10/17/17.
+ */
+public class TooLargeTooSmall {
+    private Integer chosenNum;
+
+
+
+    public TooLargeTooSmall(Integer seed) {
+        // initialise instance variables
+        chosenNum = seed;
+
+
+    }
+
+    public Integer guess(Integer g) {
+        if (g < chosenNum) {
+            return -1;
+        } else if (g > chosenNum) {
+            return 1;
+        } else {
+            return 0;
+        }
+
+
+
+
+
+    }
+}
